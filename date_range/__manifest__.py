816d102873c01b310d378789f8c43c9eaf8568c6
MacBook <macbook@MacBook-Pro-de-MacBook.local>
2020-11-25 13:36:10 +0100

    Tresorerie

--- date_range/__manifest__.py
+++ date_range/__manifest__.py
@@ -1,17 +1,15 @@
-# -*- coding: utf-8 -*-
-
+# Copyright 2016 ACSONE SA/NV (<http://acsone.eu>)
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
 {
-    "name": "Périodes diverses",
+    "name": "Date Range",
     "summary": "Manage all kind of date range",
-    "version": "10.0",
+    "version": "14.0.1.0.0",
     "category": "Uncategorized",
-    "author": "Andema/OCA",
-    "website": "http://www.andemaconsulting.com",
-    "application": False,
+    "website": "https://github.com/oca/server-ux",
+    "author": "ACSONE SA/NV, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
     "installable": True,
-    "depends": [
-        "web",
-    ],
+    "depends": ["web"],
     "data": [
         "security/ir.model.access.csv",
         "security/date_range_security.xml",
@@ -19,7 +17,7 @@
         "views/date_range_view.xml",
         "wizard/date_range_generator.xml",
     ],
-    "qweb": [
-        "static/src/xml/date_range.xml",
-    ]
+    "qweb": ["static/src/xml/date_range.xml"],
+    "development_status": "Mature",
+    "maintainers": ["lmignon"],
 }
