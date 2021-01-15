fafcd6b2cf1d874b5d34631b40a25d971596fa7a
kasback <macbook@MacBook-Pro-de-MacBook.local>
2021-01-15 17:36:17 +0100

    Restriction vente

--- sale_extend/models/sale.py
+++ sale_extend/models/sale.py
@@ -1,13 +1,23 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-
-
-class SaleOrder(models.Model):
-    _inherit = 'sale.order'
+from odoo.exceptions import ValidationError
 
 
 class SaleOrderLine(models.Model):
     _inherit = 'sale.order.line'
 
     tag_ids = fields.Many2many('crm.tag', related="order_id.tag_ids", string='Étiquettes')
+    lot_id = fields.Many2one('stock.production.lot', 'Numéro de série')
+
+    def write(self, vals):
+        if vals.get('lot_id', False):
+            lot_id = self.env['stock.production.lot'].browse(vals['lot_id'])
+            existing_line = self.env['sale.order.line'].search([]).filtered(lambda line: line.lot_id == lot_id)
+            if not existing_line:
+                lot_id.write({
+                    'partner_id': self.order_id.partner_id.id
+                })
+            else:
+                raise ValidationError('Ce numéro de série est déjà affécté')
+        return super(SaleOrderLine, self).write(vals)
