03074bce35dc04da24a9bb59de110c445ecf0b4b
kasback <macbook@MacBook-Pro-de-MacBook.local>
2021-06-21 11:36:19 +0100

    modèle de facture

--- crm_extend/models/mail_activity.py
+++ crm_extend/models/mail_activity.py
@@ -1,9 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, tools
 from odoo.exceptions import ValidationError
-
-from odoo.odoo import tools
 
 
 class MailActivity(models.Model):
